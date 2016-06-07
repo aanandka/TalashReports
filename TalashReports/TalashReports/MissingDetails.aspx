ec280c49275b05880a98c63220232ef7192bdb96
Aanand <Aanand@AANAND-PC>
2016-06-07 16:45:06 +0530

    First Commit & Push

---
+++ TalashReports/TalashReports/MissingDetails.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MissingDetails.aspx.cs" Inherits="TalashReports.MissingDetails" %>
+
+<%@ Register Assembly="CrystalDecisions.Web, Version=13.0.2000.0, Culture=neutral, PublicKeyToken=692fbea5521e1304" Namespace="CrystalDecisions.Web" TagPrefix="CR" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+            <asp:UpdatePanel ID="UpdatePanel1" runat="server" UpdateMode="Always">
+                <ContentTemplate>
+                    <CR:CrystalReportViewer ID="CrystalReportViewer1" runat="server" AutoDataBind="true" EnableDatabaseLogonPrompt="False" HasRefreshButton="True" OnInit="CrystalReportViewer1_Init" />
+                </ContentTemplate>
+            </asp:UpdatePanel>
+        </div>
+    </form>
+</body>
+</html>
---
+++ TalashReports/TalashReports/MissingDetails.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using CrystalDecisions.CrystalReports.Engine;
+using System.Configuration;
+using System.Data;
+
+namespace TalashReports
+{
+    public partial class MissingDetails : System.Web.UI.Page
+    {
+        
+        protected void CrystalReportViewer1_Init(object sender, EventArgs e)
+        {
+            MySqlConnection con = null;
+            MySqlDataAdapter da = null;
+            DataSet ds = null;
+            ReportDocument rpt = null;
+
+            con = new MySqlConnection(ConfigurationManager.ConnectionStrings["TalashConnection"].ConnectionString);
+            da = new MySqlDataAdapter("Select * from MissingPersonDetail ", con);
+            ds = new DataSet();
+            da.Fill(ds);
+            rpt = new ReportDocument();
+
+            rpt.Load(Server.MapPath("MissingDetailsReport.rpt"));
+            rpt.SetDataSource(ds.Tables[0]);
+            rpt.SetDatabaseLogon("root", "Nerd@9257", "10.23.72.174", "talash");
+
+            CrystalReportViewer1.ReportSource = rpt;
+            CrystalReportViewer1.DataBind();
+
+        }
+    }
+}
